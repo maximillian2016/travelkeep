37f44b09822330e9f861740e61a16c44d51ce27b
Robert <rbruce155@gmail.com>
2016-07-29 08:26:48 -0700

    updated dbschema

--- travelkeepdb.sql
+++ travelkeepdb.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema travelkeepdb
 -- -----------------------------------------------------
 
@@ -15,48 +18,163 @@ CREATE SCHEMA IF NOT EXISTS `travelkeepdb` DEFAULT CHARACTER SET utf8 ;
 USE `travelkeepdb` ;
 
 -- -----------------------------------------------------
--- Table `travelkeepdb`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `travelkeepdb`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  `password` VARCHAR(255) NULL,
-  `created_at` DATETIME NULL,
-  `updated_at` DATETIME NULL,
-  `share_w_friends` TINYINT(1) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `travelkeepdb`.`trips`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `travelkeepdb`.`trips` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL DEFAULT NULL,
   `start_date` DATE NOT NULL,
   `end_date` DATE NOT NULL,
-  `rating` TINYINT(1) NULL,
-  `start_location` VARCHAR(45) NULL,
-  `end_location` VARCHAR(45) NULL,
-  `trip_miles` INT NULL,
+  `rating` TINYINT(1) NULL DEFAULT NULL,
+  `start_location` VARCHAR(45) NULL DEFAULT NULL,
+  `end_location` VARCHAR(45) NULL DEFAULT NULL,
+  `trip_miles` INT(11) NULL DEFAULT NULL,
+  `trip_complete` INT(1) NULL DEFAULT '0',
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 13
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `travelkeepdb`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `travelkeepdb`.`users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NULL DEFAULT NULL,
+  `last_name` VARCHAR(45) NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `password` VARCHAR(255) NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  `share_w_friends` TINYINT(1) NULL DEFAULT NULL,
+  `picture` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 13
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `travelkeepdb`.`comments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `travelkeepdb`.`comments` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `content` TEXT NULL DEFAULT NULL,
+  `user_id` INT(11) NOT NULL,
+  `trip_id` INT(11) NOT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_comments_users1_idx` (`user_id` ASC),
+  INDEX `fk_comments_trips1_idx` (`trip_id` ASC),
+  CONSTRAINT `fk_comments_trips1`
+    FOREIGN KEY (`trip_id`)
+    REFERENCES `travelkeepdb`.`trips` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comments_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `travelkeepdb`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `travelkeepdb`.`favorites`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `travelkeepdb`.`favorites` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `trip_id` INT(11) NOT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  `favorite_status` TINYINT(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_favorites_users1_idx` (`user_id` ASC),
+  INDEX `fk_favorites_trips1_idx` (`trip_id` ASC),
+  CONSTRAINT `fk_favorites_trips1`
+    FOREIGN KEY (`trip_id`)
+    REFERENCES `travelkeepdb`.`trips` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_favorites_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `travelkeepdb`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 9
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `travelkeepdb`.`friends`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `travelkeepdb`.`friends` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  `user_id` INT(11) NOT NULL,
+  `friend_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_friends_users_a_idx` (`user_id` ASC),
+  INDEX `fk_friends_users_b_idx` (`friend_id` ASC),
+  CONSTRAINT `fk_friends_users1_b`
+    FOREIGN KEY (`friend_id`)
+    REFERENCES `travelkeepdb`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_friends_users_a`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `travelkeepdb`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `travelkeepdb`.`participants`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `travelkeepdb`.`participants` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `trip_id` INT(11) NOT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_participants_users1_idx` (`user_id` ASC),
+  INDEX `fk_participants_trips1_idx` (`trip_id` ASC),
+  CONSTRAINT `fk_participants_trips1`
+    FOREIGN KEY (`trip_id`)
+    REFERENCES `travelkeepdb`.`trips` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_participants_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `travelkeepdb`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 22
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `travelkeepdb`.`photos_vids`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `travelkeepdb`.`photos_vids` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `photo` VARCHAR(100) NULL,
-  `videos` VARCHAR(100) NULL,
-  `trip_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  `created_at` DATETIME NULL,
-  `updated_at` DATETIME NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `photo` VARCHAR(100) NULL DEFAULT NULL,
+  `videos` VARCHAR(100) NULL DEFAULT NULL,
+  `trip_id` INT(11) NOT NULL,
+  `user_id` INT(11) NOT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_photos_vids_trips1_idx` (`trip_id` ASC),
   INDEX `fk_photos_vids_users1_idx` (`user_id` ASC),
@@ -70,135 +188,9 @@ CREATE TABLE IF NOT EXISTS `travelkeepdb`.`photos_vids` (
     REFERENCES `travelkeepdb`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `travelkeepdb`.`comments`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `travelkeepdb`.`comments` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `content` TEXT NULL,
-  `user_id` INT NOT NULL,
-  `trip_id` INT NOT NULL,
-  `created_at` DATETIME NULL,
-  `updated_at` DATETIME NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_comments_users1_idx` (`user_id` ASC),
-  INDEX `fk_comments_trips1_idx` (`trip_id` ASC),
-  CONSTRAINT `fk_comments_users1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `travelkeepdb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_comments_trips1`
-    FOREIGN KEY (`trip_id`)
-    REFERENCES `travelkeepdb`.`trips` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `travelkeepdb`.`friends`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `travelkeepdb`.`friends` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `created_at` DATETIME NULL,
-  `updated_at` DATETIME NULL,
-  `user_id` INT NOT NULL,
-  `friend_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_friends_users_a_idx` (`user_id` ASC),
-  INDEX `fk_friends_users_b_idx` (`friend_id` ASC),
-  CONSTRAINT `fk_friends_users_a`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `travelkeepdb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_friends_users1_b`
-    FOREIGN KEY (`friend_id`)
-    REFERENCES `travelkeepdb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `travelkeepdb`.`favorites`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `travelkeepdb`.`favorites` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  `trip_id` INT NOT NULL,
-  `created_at` DATETIME NULL,
-  `updated_at` DATETIME NULL,
-  `favorite_status` TINYINT(1) NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_favorites_users1_idx` (`user_id` ASC),
-  INDEX `fk_favorites_trips1_idx` (`trip_id` ASC),
-  CONSTRAINT `fk_favorites_users1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `travelkeepdb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_favorites_trips1`
-    FOREIGN KEY (`trip_id`)
-    REFERENCES `travelkeepdb`.`trips` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `travelkeepdb`.`favorites`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `travelkeepdb`.`favorites` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  `trip_id` INT NOT NULL,
-  `created_at` DATETIME NULL,
-  `updated_at` DATETIME NULL,
-  `favorite_status` TINYINT(1) NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_favorites_users1_idx` (`user_id` ASC),
-  INDEX `fk_favorites_trips1_idx` (`trip_id` ASC),
-  CONSTRAINT `fk_favorites_users1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `travelkeepdb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_favorites_trips1`
-    FOREIGN KEY (`trip_id`)
-    REFERENCES `travelkeepdb`.`trips` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `travelkeepdb`.`participants`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `travelkeepdb`.`participants` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  `trip_id` INT NOT NULL,
-  `created_at` DATETIME NULL,
-  `updated_at` DATETIME NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_participants_users1_idx` (`user_id` ASC),
-  INDEX `fk_participants_trips1_idx` (`trip_id` ASC),
-  CONSTRAINT `fk_participants_users1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `travelkeepdb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_participants_trips1`
-    FOREIGN KEY (`trip_id`)
-    REFERENCES `travelkeepdb`.`trips` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
